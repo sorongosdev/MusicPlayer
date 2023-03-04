2fc036b88e6adab02230638f200b60eb7c9c05f0
Sorongos <dosora9@gmail.com>
2023-03-04 17:33:01 +0900

    mp3 삽입, 클릭시 음원 재생

--- app/src/main/java/com/sorongos/musicplayer/MainActivity.kt
+++ app/src/main/java/com/sorongos/musicplayer/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.sorongos.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sorongos.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,12 +19,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        //메모리 해제
+        mediaPlayer?.release()
+        mediaPlayer = null
+
     }
 
     private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+
     }
 
     private fun mediaPlayerPlay() {
-
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.walkaway).apply{
+                //반복 재생
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
     }
 }
